f7982ade9b22927fd25781c7bab9bae330148ff8
Andy Anderson <andyanderson1818@gmail.com>
2017-08-22 14:34:24 -0700

    Added

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  createFish(event){
+    event.preventDefault();
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value,
+    }
+    this.props.addFish(fish);
+    this.fishForm.reset();
+  }
+
+
+
+  render(){
+    return(
+      <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+        <input ref={(input)=>this.name=input}type="text" placeholder="Fish Name" />
+        <input ref={(input)=>this.price=input}type="text" placeholder="Fish Price" />
+          <select>
+            <option value="Available"> Fresh! </option>
+            <option value="Unavailable"> Sold Out! </option>
+          </select>
+        <input ref={(input)=>this.status=input}type="text" placeholder="Fish Status" />
+        <textarea ref={(input)=>this.desc=input}type="text" placeholder="Fish Description" />
+        <input ref={(input)=>this.image=input}type="text" placeholder="Fish Image" />
+        <button type="submit">+Add to Order</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
