32103a13ace25799716f7b6f1a7d6895051d2bb9
Andy Anderson <andyanderson1818@gmail.com>
2017-08-25 13:45:36 -0700

    Added basejs

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBfEK3E3YtklNkBJeoUZ-76zFIi44jFIM8",
+    authDomain: "reactbos.firebaseapp.com",
+    databaseURL: "https://reactbos.firebaseio.com"
+});
+
+export default base;
